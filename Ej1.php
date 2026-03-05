8c2840013daec1efc94e0b50f0438972b79ff7be
RubenMMPS <rmartinmotilla@gmail.com>
2026-03-05 11:20:14 +0100

    Ejercicio 1

---
+++ Ej1.php
@@ -0,0 +1,14 @@
+Ejercicio 1, Nomenclatura y tipo de variables
+Escribe una varible llamada user name utilizando las siguientes nomenclaturas:
+Camel case:userName
+Pascal case:UserName
+Snake case:user_name
+Kebab case:user-name
+Define los siguientes tipos de variables, di cuál utilizarías y por qué.
+Var: Es como se definian las variables antiguamente en javascript, sus variables pueden ser reasignadas  su alcance es la función en la que se declaran o elk objeto global.
+Let: Es una forma más moderna de declarar las variables en javascript, sus variables pueden ser reasignadas pero su alcance está limitado al bloque de código en el que se declaran.
+Const: También es una forma más moderna de declarar variables en javascript, pero sus variables no pueden ser reasignadas después de incializarlas, su alcance está limitado al bloque de código en el que se declaran.
+
+Utilizaria var en el caso de que necesite una variable que pueda ser reasignada y que tenga un alcance global o tenga una versión de javascript anitgua .
+Utilizaría let para variables que necesitan ser reasignadas dentro de un bloque específico.Let sería la que más usaria porque podría reasignar el valor de las variables y además sería una opción más moderna que var.
+Utilizaría const para variables que no necesitan ser reasignadas después de su inicialización, como valores constantes o referencias a objetos que no deben cambiar.
