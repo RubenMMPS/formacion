706fe74ec156215705a66f5780480850cee1848a
RubenMMPS <rmartinmotilla@gmail.com>
2026-03-05 12:03:24 +0100

    Ejercicio 4

---
+++ Ej4.js
@@ -0,0 +1,59 @@
+/* Ejercicio 4, refactoriza el código
+Dado los siguientes códigos, refactorízalos como consideres, si consideras que está bien, explica el motivo. 
+
+Primer código
+Tenemos una función que saca el nombre completo del usuario con el nombre y apellido */
+
+class usuario {
+  constructor(
+    nombre,
+    apellido,
+    edad,
+    profesion,
+    salario = 0,
+    nacionalidad = "España",
+  ) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.edad = edad;
+    this.profesion = profesion;
+    this.salario = salario;
+    this.nacionalidad = nacionalidad;
+  }
+
+  nombreUsuario() {
+    const nombrCompleto = "Me llamo " + this.nombre + " " + this.apellido;
+
+    return nombrCompleto;
+  }
+
+  userData() {
+    const annualSalary = this.salario * 12;
+    const fullName = `${this.nombre} ${this.apellido}`;
+
+    return `Me llamo ${fullName} y cobro ${annualSalary}€ al año`;
+  }
+
+  esExtrangero() {
+    if (this.nacionalidad != "España" && this.edad == "30") {
+      return "Apto para la ayuda del gobierno";
+    } else {
+      return "No es apto para la ayuda del gobierno";
+    }
+  }
+}
+var presidente = new usuario("Pedro", "Sánchez", 45, "Barro man", 1000);
+
+console.log(presidente.nombreUsuario());
+
+/* Segundo código
+Tenemos una función que calcula el salario anual y el nombre del usuario */
+
+console.log(presidente.userData());
+
+/* Tercer código
+Si el usuario tiene 30 años y no es español, recibirá una ayuda del gobierno de 2000€ */
+
+user = new usuario("Álvaro", "Morón", 30, "", 0, "Morocco");
+
+console.log(user.esExtrangero());
