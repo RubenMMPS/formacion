2597485dd0463d00b11b719f54c6dc2b44535242
RubenMMPS <rmartinmotilla@gmail.com>
2026-03-09 17:25:39 +0100

    feat(VUE) add exercises 4 and 5

---
+++ VUE/nuxt-vue-exercises/components/TheCounter.vue
@@ -0,0 +1,26 @@
+<template>
+  <TheTitle> Contador</TheTitle>
+  <div>
+    <h2>Valor actual = {{ counter }}</h2>
+
+    <button v-if="counter > 0" v-on:click="decrement">decrement</button>
+
+    <button v-if="counter < 10" v-on:click="increment">increment</button>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const counter = ref(0);
+
+const increment = () => {
+  counter.value++;
+};
+
+const decrement = () => {
+  if (counter.value > 0) {
+    counter.value--;
+  }
+};
+</script>
--- VUE/nuxt-vue-exercises/pages/index.vue
+++ VUE/nuxt-vue-exercises/pages/index.vue
@@ -1,7 +1,11 @@
+<script setup lang="ts">
+import TheCounter from "~/components/TheCounter.vue";
+</script>
+
 <template>
   <div>
     <TheTitle> Mi Título con Slots </TheTitle>
 
-    <TheTitle></TheTitle>
+    <TheCounter></TheCounter>
   </div>
 </template>
