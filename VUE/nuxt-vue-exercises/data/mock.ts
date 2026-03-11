4112882ef2106d54e6de7113cdc7f6e876505426
RubenMMPS <rmartinmotilla@gmail.com>
2026-03-11 09:52:59 +0100

    feat(VUE) add exercise 16

--- VUE/nuxt-vue-exercises/data/mock.ts
+++ VUE/nuxt-vue-exercises/data/mock.ts
@@ -1,51 +1,35 @@
-class Product {
+interface Product {
   id: number;
   title: string;
   description: string;
   price: number;
-  img: string;
-  button: string;
-
-  constructor(
-    id: number,
-    title: string,
-    description: string,
-    price: number,
-    img: string = "",
-    button: string = "Buy Now",
-  ) {
-    this.id = id;
-    this.title = title;
-    this.description = description;
-    this.price = price;
-    this.img = img;
-    this.button = button;
-  }
+  img?: string;
+  button?: string;
 }
 
 export const products: Product[] = [
-  new Product(
-    1,
-    "Laptop",
-    "A high-performance laptop for all your needs.",
-    999.99,
-  ),
-  new Product(
-    2,
-    "Smartphone",
-    "A sleek smartphone with the latest features.",
-    499.99,
-  ),
-  new Product(
-    3,
-    "Headphones",
-    "Noise-cancelling headphones for immersive sound.",
-    199.99,
-  ),
-  new Product(
-    4,
-    "Smartwatch",
-    "A stylish smartwatch to keep you connected.",
-    299.99,
-  ),
+  {
+    id: 1,
+    title: "Laptop",
+    description: "A high-performance laptop for all your needs.",
+    price: 999.99,
+  },
+  {
+    id: 2,
+    title: "Smartphone",
+    description: "A sleek smartphone with the latest features.",
+    price: 499.99,
+  },
+  {
+    id: 3,
+    title: "Headphones",
+    description: "Noise-cancelling headphones for immersive sound.",
+    price: 199.99,
+  },
+  {
+    id: 4,
+    title: "Smartwatch",
+    description: "A stylish smartwatch to keep you connected.",
+    price: 299.99,
+  },
 ];
