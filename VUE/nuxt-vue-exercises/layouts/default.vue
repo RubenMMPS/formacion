a1efa11caa11195a98d5d2e684310c3c7ab63bd3
RubenMMPS <rmartinmotilla@gmail.com>
2026-03-11 10:28:21 +0100

    feat(VUE) add exercise 17

--- VUE/nuxt-vue-exercises/layouts/default.vue
+++ VUE/nuxt-vue-exercises/layouts/default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="min-h-screen flex flex-col">
     <header class="flex items-center gap-4 p-4 bg-gray-100 shadow">
       <NuxtLink to="/home"><h1>Nuxt 4 Project</h1></NuxtLink>
       <div class="ml-auto flex items-center gap-4">
@@ -8,6 +8,14 @@
         <NuxtLink to="/counter"><button>Contador</button></NuxtLink>
       </div>
     </header>
-    <slot />
+    <main class="flex-grow container mx-auto p-4">
+      <slot />
+    </main>
+    <footer
+      class="flex items-center justify-center p-4 bg-gray-100 shadow mt-8"
+    >
+      <p>© 2026 Nuxt 4 Project. All rights reserved.</p>
+      <Icons></Icons>
+    </footer>
   </div>
 </template>
