83820b1cc28173cd4b90dd9db6479da957c00eb2
RubenMMPS <rmartinmotilla@gmail.com>
2026-03-11 11:09:19 +0100

    refactor(VUE) move the header from the default layout to a separate component

---
+++ VUE/nuxt-vue-exercises/components/Header.vue
@@ -0,0 +1,12 @@
+<template>
+  <header>
+    <nav class="flex items-center gap-4 p-4 bg-gray-100 shadow">
+      <NuxtLink to="/home"><h2>Menú</h2></NuxtLink>
+      <div class="ml-auto flex items-center gap-4">
+        <NuxtLink to="/home"><button>Home</button></NuxtLink>
+        <NuxtLink to="/catalogo"><button>Catalogo</button></NuxtLink>
+        <NuxtLink to="/counter"><button>Contador</button></NuxtLink>
+      </div>
+    </nav>
+  </header>
+</template>
--- VUE/nuxt-vue-exercises/layouts/default.vue
+++ VUE/nuxt-vue-exercises/layouts/default.vue
@@ -1,15 +1,6 @@
 <template>
   <div class="min-h-screen flex flex-col">
-    <header>
-      <nav class="flex items-center gap-4 p-4 bg-gray-100 shadow">
-        <NuxtLink to="/home"><h2>Menú</h2></NuxtLink>
-        <div class="ml-auto flex items-center gap-4">
-          <NuxtLink to="/home"><button>Home</button></NuxtLink>
-          <NuxtLink to="/catalogo"><button>Catalogo</button></NuxtLink>
-          <NuxtLink to="/counter"><button>Contador</button></NuxtLink>
-        </div>
-      </nav>
-    </header>
+    <Header></Header>
     <main class="flex-grow container mx-auto p-4">
       <slot />
     </main>
